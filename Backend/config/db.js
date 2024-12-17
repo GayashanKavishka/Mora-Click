e65f5b809032e3ba24881d44987ec7d495492cb9
W.G Kavishka Gayashan De Silva <167881650+GayashanKavishka@users.noreply.github.com>
2024-12-17 14:17:04 +0530

    ff

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+const url = `mongodb+srv://${username}:${password}@cluster0.qucvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Corrected connection string
 // const db = mongoose.connect('mongodb+srv://gayashankavishka2:Gaya@12345@cluster0.qucvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 const db = mongoose.connect(
-    'mongodb+srv://gayashankavishka2:Gaya12345@cluster0.qucvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-);
+    // 'mongodb+srv://gayashankavishka2:Gaya12345@cluster0.qucvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    url
 
-db.then(() => {
+).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.log('Error connecting to MongoDB', err);
