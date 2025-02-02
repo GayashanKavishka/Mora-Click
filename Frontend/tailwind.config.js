23dfa77e474aa17ab5f7e9b919f8510257ca4a4e
W.G Kavishka Gayashan De Silva <167881650+GayashanKavishka@users.noreply.github.com>
2025-02-02 11:52:50 +0530

    Make responsive and add some new to welcome page

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
         "light-green": "#c8d8be",
         "orange-main": "#ff8c42",
       },
+      keyframes: {
+        slideInLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-in-left': 'slideInLeft 1s ease-out',
+      },
+      colors: {
+        'custom-yellow': '#f1c40f',
+        'custom-orange': '#e67e22',
+      },
     },
   },
   plugins: [],
