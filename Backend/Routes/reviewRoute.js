e84b1d352d9ab4653d9f34744378107cd1d2744b
Dineth Danurdha <163334443+danux2002@users.noreply.github.com>
2025-02-03 21:21:11 +0530

    Review submisson

--- Backend/Routes/reviewRoute.js
+++ Backend/Routes/reviewRoute.js
@@ -2,12 +2,16 @@ const router = require('express').Router();
 
 
 const { addReview } = require('../controllers/reviewcontroller');
+
 router.get('/getreview', async (req, res) => {});
 router.put('/updatereview', async (req, res) => {});
 
 router.post('/addreview', async (req, res) => {
+
+    console.log(req.body);
     try {
         const result = await addReview(req.body);
+        
         return res.status(result.status).json({ message: "Review added successfully", data: result.data });
     } catch (error) {
         console.error(error);
--- Backend/controllers/reviewcontroller.js
+++ Backend/controllers/reviewcontroller.js
@@ -5,6 +5,9 @@ const reviews = require('../model/reviewModel');
 const addReview = (obj) => {
     return new Promise((resolve, reject) => {
         const { canteenId, userId, review } = obj;
+        console.log('canteenId:', canteenId);
+        console.log('userId:', userId);
+        console.log('review:', review);
         const newReview = new reviews({
             _id: new mongo.Types.ObjectId(),
             canteenId,
@@ -13,6 +16,7 @@ const addReview = (obj) => {
         });
 
         newReview.save().then((data) => {
+            console.log('Review added successfully:', data);
             return resolve({ status: 200, data });
         }).catch((err) => {
             return reject(err);
