546d261730f6df37dd2ef768dff48ccf8fef18c2
Yasith Imalka <157888522+Yasith763@users.noreply.github.com>
2025-02-04 17:38:34 +0530

    update sign up

--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./SignUp.css";
+import bg from "../assets/bgi.jpg";
 
 const SignUp = () => {
   const navigate = useNavigate(); // Hook for navigation
