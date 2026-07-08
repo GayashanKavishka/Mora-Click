b649bac585d02770d02467947643f4bd7a96a73e
W.G Kavishka Gayashan De Silva <167881650+GayashanKavishka@users.noreply.github.com>
2026-07-08 22:28:55 +0530

    Update db.js
    sample change

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -14,7 +14,7 @@ const db = mongoose.connect(
     url
 
 ).then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB'); 
 }).catch((err) => {
     console.log('Error connecting to MongoDB', err);
 });
